349e8f7380fdfc76ac5ea01d08d32775eaa9e403
Jordi Prats <jprats@systemadmin.es>
2016-09-12 18:30:26 +0200

    update sumary

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,16 +1,20 @@
-define redis::instance($port=$name) {
+define redis::instance(
+                        $port = $name
+                      ) {
 
-  # include systemd
-  #
-  # systemd::service { $instancename:
-  #   execstart => "/etc/init.d/${instancename} start",
-  #   execstop  => "/etc/init.d/${instancename} stop",
-  #   require   => File["/etc/init.d/${instancename}"],
-  #   before    => Service[$redis::params::servicename],
-  #   forking   => true,
-  #   restart   => 'no',
-  #   user      => 'tomcat',
-  #   group     => 'tomcat',
-  # }
-
+  if($redis::params::systemd)
+  {
+    # include systemd
+    #
+    # systemd::service { $instancename:
+    #   execstart => "/etc/init.d/${instancename} start",
+    #   execstop  => "/etc/init.d/${instancename} stop",
+    #   require   => File["/etc/init.d/${instancename}"],
+    #   before    => Service[$redis::params::servicename],
+    #   forking   => true,
+    #   restart   => 'no',
+    #   user      => 'tomcat',
+    #   group     => 'tomcat',
+    # }
+  }
 }
