1d7d6331fdad7d4070f730f16569a5e02bc077bb
Jordi Prats <jprats@systemadmin.es>
2016-09-12 18:11:34 +0200

    afegit script systemd

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,3 +1,16 @@
 define redis::instance($port=$name) {
 
+  # include systemd
+  #
+  # systemd::service { $instancename:
+  #   execstart => "/etc/init.d/${instancename} start",
+  #   execstop  => "/etc/init.d/${instancename} stop",
+  #   require   => File["/etc/init.d/${instancename}"],
+  #   before    => Service[$redis::params::servicename],
+  #   forking   => true,
+  #   restart   => 'no',
+  #   user      => 'tomcat',
+  #   group     => 'tomcat',
+  # }
+
 }
