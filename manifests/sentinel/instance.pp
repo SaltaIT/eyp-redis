cf4bb5c973baac6794bf19e90c81f2f67144e120
Jordi Prats <jordi.prats@gmail.com>
2018-04-25 13:37:51 +0200

    lint

--- manifests/sentinel/instance.pp
+++ manifests/sentinel/instance.pp
@@ -1,14 +1,14 @@
-define redis::sentinel::instance (
-                            $instance_name           = $name,
-                            $redis_port              = $name,
-                            $redis_password          = undef,
-                            $order                   = '42',
-                            $group_name              = "${name}cluster",
-                            $quorum                  = '2',
-                            $down_after_milliseconds = '5000',
-                            $parallel_syncs          = '1',
-                            $failover_timeout        = '10000',
-                          ) {
+define redis::sentinel::instance(
+                                  $instance_name           = $name,
+                                  $redis_port              = $name,
+                                  $redis_password          = undef,
+                                  $order                   = '42',
+                                  $group_name              = "${name}cluster",
+                                  $quorum                  = '2',
+                                  $down_after_milliseconds = '5000',
+                                  $parallel_syncs          = '1',
+                                  $failover_timeout        = '10000',
+                                ) {
   include redis::sentinel
 
   concat::fragment { "sentinel ${instance_name}":
