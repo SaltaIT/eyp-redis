83cf4a03b0fdfbb6c0f7866de7183b552b8b036c
Jordi Prats <jordi.prats@gmail.com>
2018-04-25 17:24:49 +0200

    test mes instancies

--- examples/sentinel.pp
+++ examples/sentinel.pp
@@ -8,6 +8,17 @@ redis::instance { '1112':
   sentinel => true,
 }
 
-redis::instance { '1113': }
+redis::instance { '1113':
+}
 
 redis::instance { '1114': }
+
+redis::instance { '1115':
+  sentinel => true,
+  sentinel_group_name => 'testcluster',
+}
+
+redis::instance { '1116':
+  sentinel => true,
+  sentinel_group_name => 'testcluster',
+}
